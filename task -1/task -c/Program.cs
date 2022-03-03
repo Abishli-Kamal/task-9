5543a0fb1b84b5feb7cb5bdaf3ad25b9207a254e
Abishli-Kamal <tu7ldxfzy@code.edu.az>
2022-03-03 22:53:42 +0400

    C# başladıq inşallah uğurlu olar :)

---
+++ task -1/task -c/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task__c
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number%2==0)
+            {
+                Console.WriteLine("cut ededdir");
+            }
+            else if(number%2==1)
+            {
+                Console.WriteLine("tek ededdir");
+            }
+        }
+    }
+}
---
+++ task-2/task-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1>num2)
+            {
+                Console.WriteLine("num1 boyukdur");
+            }
+            else if(num1<num2)
+            {
+                Console.WriteLine("num2 boyukdur");
+            }
+            else
+            {
+                Console.WriteLine("beraberdir");
+            }
+        }
+    }
+}
